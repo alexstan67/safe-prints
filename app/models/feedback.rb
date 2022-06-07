aea132882acb647b31f75d7a93ab45c9b09f7825
glen smith <glensmith115@gmail.com>
2022-06-07 15:15:47 +0100

    associataions and validations completed for models

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,6 @@
 class Feedback < ApplicationRecord
   belongs_to :user
   belongs_to :report
+  validates :comment, length: { maximum: 200 }
+  validates :votes, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reports
+  has_many :feedbacks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :country, :email, presence: true
+  validates :email, uniqueness: true
 end
