9ed4c27ad03d1095095b45a9d95a77544f96a11f
glen smith <glensmith115@gmail.com>
2022-06-07 12:33:56 +0100

    created report model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_105459) do
+ActiveRecord::Schema.define(version: 2022_06_07_113126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reports", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "category"
+    t.integer "risk_level"
+    t.text "description"
+    t.datetime "report_date_time"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +44,5 @@ ActiveRecord::Schema.define(version: 2022_06_07_105459) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reports", "users"
 end
