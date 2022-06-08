65cadd40f475d52fd3a2be4a126c127bd096a01d
Alexandre Stanescot <alex@aerostan.com>
2022-06-08 11:46:59 +0800

    feat: seeds first version

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,44 +10,47 @@ require 'faker'
 
 # Cleaning the database
 puts "Cleaning the database..."
-User.destroy_all
 Feedback.destroy_all
 Report.destroy_all
+User.destroy_all
 
 # Create teams users
 real_users = []
 
 puts "Create team user Riza..."
 user = User.new
-user.first-name = "Riza"
+user.first_name = "Riza"
 user.last_name = "Santoso"
 user.email = "rizza.santoso@gmail.com"
+user.password = "12345678"
 user.country = "🇮🇩"
-file = File.open(Rails.root.join("app/assets/images/riza.jpg"))
-user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# file = File.open(Rails.root.join("app/assets/images/riza.png"))
+# user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 user.save
 real_users << user.id
 
 puts "Create team user Alex..."
 user = User.new
-user.first-name = "Alex"
+user.first_name = "Alex"
 user.last_name = "Stan"
 user.email = "alex.stan@gmail.com"
+user.password = "12345678"
 user.country = "🇫🇷"
-file = File.open(Rails.root.join("app/assets/images/alex.jpg"))
-user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# file = File.open(Rails.root.join("app/assets/images/alex.jpg"))
+# user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 user.save
 real_users << user.id
 
 
 puts "Create team user Glen..."
 user = User.new
-user.first-name = "Glen"
+user.first_name = "Glen"
 user.last_name = "Smith"
 user.email = "glen.smith@gmail.com"
+user.password = "12345678"
 user.country = "🇬🇧"
-file = File.open(Rails.root.join("app/assets/images/glen.jpg"))
-user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# file = File.open(Rails.root.join("app/assets/images/glen.jpg"))
+# user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 user.save
 real_users << user.id
 
@@ -55,18 +58,19 @@ real_users << user.id
 random_users = []
 i = 0
 10.times do
-  i++
-  puts "Create random user #{i}..." 
+  puts "Create random user #{i + 1}..." 
   user = User.new
   user.first_name = Faker::Name.first_name
   user.last_name = Faker::Name.last_name
   user.email = "#{user.first_name}.#{user.last_name}@gmail.com"
+  user.password = "12345678"
   user.country = Faker::Nation.flag
-  file = File.open(Rails.root.join(Faker::Avatar.image(slug: "my-own-slug", size: "50x50")))
-  user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  # file = File.open(Rails.root.join(Faker::Avatar.image(slug: "my-own-slug", size: "50x50")))
+  # user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
   user.save
   random_users << user.id
-en
+  i += 1
+end
 
 CATEGORIES = ["road accident", "mugging", "pickpocket", "sexual harrasment", "scams", "others"]
 LOCAL_ADDRESS = ["Jalan Batu Mejan, Canggu, Kuta Utara, Canggu, Kec. Kuta Utara, Kabupaten Badung, Bali 80351", "Jl. Pantai Batu Bolong, Canggu, Kec. Kuta Utara, Kabupaten Badung, Bali", "Jl. Pantai Batu Mejan, Canggu, Kec. Kuta Utara, Kabupaten Badung, Bali 80361", "Jl. Pantai Batu Bolong No.168, Canggu, Kec. Kuta Utara, Bandung, Bali 80351", "Jl. Pantai Batu Bolong No.89x, Canggu, Kec. Kuta Utara, Kabupaten Badung, Bali"]
@@ -75,14 +79,17 @@ LOCAL_ADDRESS = ["Jalan Batu Mejan, Canggu, Kuta Utara, Canggu, Kec. Kuta Utara,
 reports = []
 i = 0
 LOCAL_ADDRESS.each do |address|
-  i++
+  i += 1
   puts "Create local area report #{i}..."
   report = Report.new
-  report.user_id = real_user[i % 4].id
+  report.user_id = real_users[i % 3]
   report.category = CATEGORIES.sample
   report.risk_level = [0,1].sample
   report.description = "This place is dangerous, run away!"
+  report.report_date_time = Date.today
   report.address = address
+  report.latitude = 40.0
+  report.longitude = 6.0
   report.save
   reports << report
 end
@@ -90,16 +97,17 @@ end
 # Create world reports to display on main map, will be linked to random users
 i = 0
 random_users.each do |user|
-  i++
+  i += 1
   puts "Create world area report #{i}..."
   report = Report.new
-  report.user_id = user.id
+  report.user_id = user
   report.category = CATEGORIES.sample
   report.risk_level = [0,1].sample
   report.description = "This place is dangerous, run away!"
+  report.report_date_time = Date.today
   report.address = "*"
-  report.lat = Faker::Address.latitude
-  report.lon = Faker::Address.longitude
+  report.latitude = Faker::Address.latitude
+  report.longitude = Faker::Address.longitude
   report.save
   reports << report
 end
@@ -107,15 +115,16 @@ end
 # Create reviews for the real users reports
 i = 0
 reports.each do |report|
-  i++
+  i += 1
   puts "Create review #{i}..."
   feedback = Feedback.new
   feedback.user_id = real_users.sample
   feedback.comment = Faker::Lorem.sentence
   feedback.votes = [0, 1].sample
   feedback.report_id = report.id
+  feedback.save
 end
 
 puts "#{User.all.count} created!"
-puts "#{Review.all.count} created!"
+puts "#{Report.all.count} created!"
 puts "#{Feedback.all.count} created!"
