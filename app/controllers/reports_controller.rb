5253ec259d9a095a2a6dfeb5548e99c34fefa7bc
Riza Santoso <rsantoso1001@gmail.com>
2022-06-09 15:00:49 +0800

    incident report form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,38 @@
 class ReportsController < ApplicationController
+  def show
+    @report = Report.find(params[:id])
+  end
+
   def new
+    @report = Report.new
   end
 
   def create
+    @report = Report.new(report_params)
+    @report.user_id = current_user
+
+    if @report.save
+      redirect_to report_path(@report)
+    else
+      render 'new'
+    end
   end
 
-  def show
+  private
+
+  def report_params
+    params.require(:report).permit(:category, :risk_level, :description, :report_date_time, :address)
   end
 end
+
+# (1i), :report_date_time(2i), :report_date_time(3i), :report_date_time(4i), :report_date_time(5i)
+
+# category"=>"",
+#    "risk_level"=>"",
+#    "description"=>"",
+#    "report_date_time(1i)"=>"2022",
+#    "report_date_time(2i)"=>"6",
+#    "report_date_time(3i)"=>"9",
+#    "report_date_time(4i)"=>"06",
+#    "report_date_time(5i)"=>"44",
+#    "address"=>"
